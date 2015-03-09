10a1a150e9dc97b1971ae169270fe915b603d96c
xaelis <help@genmymodel.com>
2015-03-09 16:43:09 +0000

    commit by xaelis using genmymodel: v20150309_164308

--- model-parent/model-representations/src/main/java/com/model/representations/ClientResourceAssembler.java
+++ model-parent/model-representations/src/main/java/com/model/representations/ClientResourceAssembler.java
@@ -52,7 +52,7 @@ public class ClientResourceAssembler extends BasicResourceAssembler<Client, Clie
 		ClientResource resource = super.toResource(entity);
 		
         resource.getLinks().add(
-        			linkTo(clientController).slash(entity).slash("").withRel(""));
+        			linkTo(clientController).slash(entity).slash("product").withRel("product"));
 		
 		return resource;
 	}
