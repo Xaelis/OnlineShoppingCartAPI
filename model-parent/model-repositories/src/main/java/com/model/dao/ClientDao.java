2d294cb8e0285c98c2ca7ae70cca9033e6d1e6bf
xaelis <help@genmymodel.com>
2015-03-09 15:25:09 +0000

    commit by xaelis using genmymodel: v20150309_152508

---
+++ model-parent/model-repositories/src/main/java/com/model/dao/ClientDao.java
@@ -0,0 +1,13 @@
+package com.model.repositories.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import com.model.repositories.entities.Client;
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+public interfaceClientDao extends CrudRepository<Client, Integer> {
+}
+
---
+++ model-parent/model-repositories/src/main/java/com/model/dao/ProductDao.java
@@ -0,0 +1,13 @@
+package com.model.repositories.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import com.model.repositories.entities.Product;
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+public interfaceProductDao extends CrudRepository<Product, Integer> {
+}
+
