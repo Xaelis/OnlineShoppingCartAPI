00edbf1f759c3715c31b5dd8be9f709daa75d3ea
xaelis <help@genmymodel.com>
2015-03-09 17:01:43 +0000

    commit by xaelis using genmymodel: v20150309_170142

---
+++ model-parent/model-services/src/main/java/com/model/services/ClientService.java
@@ -0,0 +1,13 @@
+package com.model.service;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+public interface ClientService {
+    ClientResource find(Integer id)
+}
