c19c748f525abdbda2ed032858f82a3aaf21a7c1
xaelis <help@genmymodel.com>
2015-03-09 17:23:02 +0000

    commit by xaelis using genmymodel: v20150309_172300

---
+++ model-parent/model-services/src/main/java/com/model/services/ClientServiceImpl.java
@@ -0,0 +1,37 @@
+package com.model.services;
+
+import java.util.List;
+import java.util.Set;
+
+import com.model.representations.ClientResource;
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+public class ClientServiceImpl implements ClientService {
+   /**
+    * <!-- begin-user-doc -->
+    * <!--  end-user-doc  -->
+    * @generated
+    */
+   ClientResource find(Integer id) {
+   }
+   
+   /**
+    * <!-- begin-user-doc -->
+    * <!--  end-user-doc  -->
+    * @generated
+    */
+   void update(Integer id, ClientResource clientResource) {
+   }
+   
+   /**
+    * <!-- begin-user-doc -->
+    * <!--  end-user-doc  -->
+    * @generated
+    */
+   ClientResource create(ClientResource clientResource) {
+   }
+}
---
+++ model-parent/model-services/src/main/java/com/model/services/ProductServiceImpl.java
@@ -0,0 +1,37 @@
+package com.model.services;
+
+import java.util.List;
+import java.util.Set;
+
+import com.model.representations.ProductResource;
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+public class ProductServiceImpl implements ProductService {
+   /**
+    * <!-- begin-user-doc -->
+    * <!--  end-user-doc  -->
+    * @generated
+    */
+   ProductResource find(Integer id) {
+   }
+   
+   /**
+    * <!-- begin-user-doc -->
+    * <!--  end-user-doc  -->
+    * @generated
+    */
+   void update(Integer id, ProductResource productResource) {
+   }
+   
+   /**
+    * <!-- begin-user-doc -->
+    * <!--  end-user-doc  -->
+    * @generated
+    */
+   ProductResource create(ProductResource productResource) {
+   }
+}
