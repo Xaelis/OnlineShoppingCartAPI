ead3ee752dbaa722faad4c985de63247d57c9ea6
Xaelis <alexis.muller@axellience.com>
2015-03-12 10:28:39 +0100

    add save

--- ClientController.js
+++ ClientController.js
@@ -1,31 +1,34 @@
-app.controller("Client", function userController($scope) {
+app.controller("clientController", function clientController($scope) {
 $scope.id = '';
 $scope.name = '';
 $scope.column = '';
 $scope.clients = [
-{id:val
-, name:val
-, column:val
-}
+{id:1, name:'Toto', column:100},
+{id:2, name:'Titi', column:150},
 ];
 
-$scope.edit = true;
+$scope.newClient = true;
 $scope.error = false;
 $scope.incomplete = false; 
 
 $scope.editClient = function(id) {
   if (id == 'new') {
-    $scope.edit = true;
+    $scope.newClient = true;
     $scope.incomplete = true;
     $scope.id = '';
     $scope.name = '';
     $scope.column = '';
     } else {
-    $scope.edit = false;
+    $scope.newClient = false;
     $scope.id = $scope.clients[id-1].id;
     $scope.name = $scope.clients[id-1].name;
     $scope.column = $scope.clients[id-1].column;
   }
+};
+
+$scope.saveClient = function() {
+  $scope.clients[$scope.id-1].name = $scope.name;
+  $scope.clients[$scope.id-1].column = $scope.column;
 };
 
 //$scope.$watch('passw1',function() {$scope.test();});
@@ -41,11 +44,7 @@ $scope.test = function() {
     $scope.error = false;
   }*/
   $scope.incomplete = false;
-  if ($scope.edit && (
-  !$scope.id.length
-||  !$scope.name.length
-||  !$scope.column.length
-)) {
+  if ($scope.newClient && (!$scope.id.length ||  !$scope.name.length ||  !$scope.column.length)) {
        $scope.incomplete = true;
   }
 };
